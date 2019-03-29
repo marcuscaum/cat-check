74e5a1e3487151e0785074c878894a4482f8b20d
marcuscaum <marcuslucas.jb@gmail.com>
2019-03-29 10:27:44 -0300

    Render different images onClick

--- src/components/ImageChooser/index.jsx
+++ src/components/ImageChooser/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import sampleSize from 'lodash.samplesize';
+import difference from 'lodash.difference';
 
 import { ImagesContainer, Image } from './index.styled';
 
@@ -7,15 +8,25 @@ const IMAGE_IDS = [...new Array(21).keys()].slice(1);
 
 const ImageChooser = () => {
   const [selectedImages, setSelectedImages] = useState([]);
+  const [imagesList, setImagesList] = useState(IMAGE_IDS);
+
+  const setRandomImages = () => {
+    const images = sampleSize(imagesList, 2);
+    setSelectedImages(images);
+  };
 
   useEffect(() => {
-    const images = sampleSize(IMAGE_IDS, 2);
-    setSelectedImages(images);
+    setRandomImages();
   }, []);
+
+  useEffect(() => {
+    const images = difference(imagesList, selectedImages);
+    setImagesList(images);
+  }, [selectedImages]);
 
   return (
     <ImagesContainer>
-      {selectedImages[0] && <Image id={selectedImages[0]} />}
+      {selectedImages[0] && <Image id={selectedImages[0]} onClick={setRandomImages} />}
       {selectedImages[1] && <Image id={selectedImages[1]} />}
     </ImagesContainer>
   );
