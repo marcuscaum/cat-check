945eee7305f4d5c320f0715c62bee8118eb13c5e
marcuscaum <marcuslucas.jb@gmail.com>
2019-03-29 08:50:29 -0300

    Add styled components and fix eslint dependencie

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Main, Container } from './index.styled';
+
+const App = () => (
+  <Main>
+    <Container>
+      <header>
+        <h1>Which of these is a cat?</h1>
+      </header>
+    </Container>
+  </Main>
+);
+
+export default App;
---
+++ src/pages/App/index.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 600px;
+  height: 100vh;
+  background: salmon;
+  display: flex;
+`;
